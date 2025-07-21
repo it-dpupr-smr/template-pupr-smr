24dff04313bda88c1371f9bdad00a19638a6ac77
it-dpupr-smr <it.dpupr.smr@gmail.com>
2025-07-21 17:05:19 +0800

    Create Model.php

---
+++ pkl-umkt/lpse/Model.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LPSE extends Model
+{
+	use HasFactory;
+
+	protected $table = 'lpse';
+
+	protected $fillable = [
+		// TODO: Isi dengan nama kolom yang boleh diisi saat store atau update data.  
+	];
+}
